d7f54799d67b0d0feeedefffe0da2a6ab324100f
Surajiitmjnu <25f2005650@ds.study.iitm.ac.in>
2026-01-13 09:34:41 +0530

    Add files via upload

---
+++ avatar.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import { Post } from "../types/post";
+export default function Avatar({ author }: { author: Post["ppmaAuthorName"] }) {
+  // const isAuthorHaveFullName = author?.node?.firstName && author?.node?.lastName
+  // const name = isAuthorHaveFullName
+  //   ? `${author.node.firstName} ${author.node.lastName}`
+  //   : author.node.name || null
+  return (
+    <div className="flex items-center">
+      {/* <div className="w-8 h-8 relative mr-4">
+        <Image
+          src={author.node.avatar.url}
+          layout="fill"
+          className="rounded-full"
+          alt={name}
+        />
+      </div> */}
+      <div className="text-md font-medium heading1">{author}</div>
+    </div>
+  );
+}
